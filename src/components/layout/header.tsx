375b825f32272e4497e60387fece5d846801fe72
Garrett <90536997+garrett-huggins@users.noreply.github.com>
2025-01-20 16:23:45 -0500

    add home link

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,22 +1,25 @@
 import Image from "next/image";
 import rocketIcon from "../../../public/icons/rocket.svg";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className="bg-background-accent h-48 flex justify-center items-center">
-      <h1 className="text-primary font-black text-5xl">
-        <span>
-          <Image
-            priority
-            src={rocketIcon}
-            alt="Rocket Logo"
-            width={36}
-            height={22}
-            className="inline"
-          />
-        </span>{" "}
-        Todo <span className="text-secondary">App</span>
-      </h1>
+      <Link href="/">
+        <h1 className="text-primary font-black text-5xl">
+          <span>
+            <Image
+              priority
+              src={rocketIcon}
+              alt="Rocket Logo"
+              width={36}
+              height={22}
+              className="inline"
+            />
+          </span>{" "}
+          Todo <span className="text-secondary">App</span>
+        </h1>
+      </Link>
     </header>
   );
 };
