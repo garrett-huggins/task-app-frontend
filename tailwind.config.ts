720c6e416cb56225dc77a682fe62c96a6506c189
Garrett <90536997+garrett-huggins@users.noreply.github.com>
2025-01-20 01:42:57 -0500

    add styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,27 @@ export default {
     extend: {
       colors: {
         background: "var(--background)",
+        "background-accent": "var(--background-accent)",
+        "background-muted": "rgb(var(--background-muted))",
         foreground: "var(--foreground)",
+        "foreground-muted": "var(--foreground-muted)",
+        muted: "var(--muted)",
+
+        primary: "var(--primary)",
+        "primary-muted": "rgb(var(--primary-muted))",
+        secondary: "var(--secondary)",
+        "secondary-muted": "var(--secondary-muted)",
+
+        "tag-none": "var(--tag-none)",
+        "tag-red": "var(--tag-red)",
+        "tag-orange": "var(--tag-orange)",
+        "tag-yellow": "var(--tag-yellow)",
+        "tag-green": "var(--tag-green)",
+        "tag-blue": "var(--tag-blue)",
+        "tag-indigo": "var(--tag-indigo)",
+        "tag-purple": "var(--tag-purple)",
+        "tag-pink": "var(--tag-pink)",
+        "tag-brown": "var(--tag-brown)",
       },
     },
   },
